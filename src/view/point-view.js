2cfc41931738b192fb021dfd5ac8651c480e13a6
Viktoria Anufrieva <anufrievavick@gmai.com>
2023-06-06 15:04:49 +0300

    module2 fixed

--- src/view/point-view.js
+++ src/view/point-view.js
@@ -1,46 +1,64 @@
 import { createElement } from '../render';
+import { getOfferName, getOfferPrice } from '../fish/offer';
+import { getDateForm, getDateTime, getTimeDate, getYearsFormat } from '../util';
+import { getCityById } from '../fish/destination';
 
-const createNewPoint = () => (
+function createOffersTemplate(offers) {
+  return offers.map((offer) => `
+    <li class="event__offer">
+      <span class="event__offer-title">${getOfferName(offer)}</span>
+      &plus;&euro;&nbsp;
+      <span class="event__offer-price">${getOfferPrice(offer)}</span>
+    </li>
+  `).join('');
+}
+
+function createNewPointTemplate(point) {
+  const {price, dateFrom, dateTo, destination, offers, type} = point;
+  const fromDate = getTimeDate(dateFrom);
+  const timeFrom = getDateTime(dateFrom);
+  const toDate = getTimeDate(dateTo);
+  const timeTo = getDateTime(dateTo);
+  const fullDate = getYearsFormat(dateFrom);
+  const shortDate = getDateForm(dateFrom);
+
+  return (
   `<li class="trip-events__item">
     <div class="event">
-      <time class="event__date" datetime="2019-03-18">MAR 18</time>
+      <time class="event__date" datetime="${fullDate}">${shortDate}</time>
       <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
       </div>
-      <h3 class="event__title">Taxi Amsterdam</h3>
+      <h3 class="event__title">${type} ${getCityById(destination)}</h3>
       <div class="event__schedule">
         <p class="event__time">
-          <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+          <time class="event__start-time" datetime="${fromDate}">${timeFrom}</time>
           &mdash;
-          <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+          <time class="event__end-time" datetime="${toDate}">${timeTo}</time>
         </p>
       </div>
       <p class="event__price">
-        &euro;&nbsp;<span class="event__price-value">20</span>
+        &euro;&nbsp;<span class="event__price-value">${price}</span>
       </p>
       <h4 class="visually-hidden">Offers:</h4>
       <ul class="event__selected-offers">
-        <li class="event__offer">
-          <span class="event__offer-title">Order Uber</span>
-          &plus;&euro;&nbsp;
-          <span class="event__offer-price">20</span>
-        </li>
+        ${createOffersTemplate(offers)}
       </ul>
       <button class="event__rollup-btn" type="button">
         <span class="visually-hidden">Open event</span>
       </button>
     </div>
-  </li>`
-);
+  </li>`);
+}
 
 
 export default class NewPointView {
-  constructor(destination){
-    this.destination = destination;
+  constructor({tripPoint}){
+    this.tripPoint = tripPoint;
   }
 
   getTemplate() {
-    return createNewPoint(this.destination);
+    return createNewPointTemplate(this.tripPoint);
   }
 
   getElement() {
